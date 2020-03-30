e41581d1151c6138923d9798aa3c4cd3cc18f85b
bseverino <biancasev@gmail.com>
2020-03-30 14:13:05 -0400

    Completed exercises 6-10

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -2,7 +2,7 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
@@ -11,13 +11,17 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for x in range(5):
+    y.append(x + 1)
 
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+for x in range(10):
+    y.append(x**3)
 
 print(y)
 
@@ -27,6 +31,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+for x in a:
+    y.append(x.upper())
 
 print(y)
 
@@ -34,6 +40,14 @@ print(y)
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
+even = []
+
+for val in x:
+    val = int(val)
+    if val % 2 == 0:
+        even.append(val)
+
+print(even)
 
 # What do you need between the square brackets to make it work?
 y = []
