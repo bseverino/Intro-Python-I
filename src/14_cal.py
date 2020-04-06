bbb3e7a663b7cf999bc2b830cd43da70b30b2932
bseverino <biancasev@gmail.com>
2020-04-06 15:38:49 -0400

    Fixed calendar syntax

--- src/14_cal.py
+++ src/14_cal.py
@@ -34,7 +34,7 @@ from datetime import datetime
 yy = datetime.now().year
 mm = datetime.now().month
 
-elif len(sys.argv) > 2 and int(sys.argv[1]) >= 1 and int(sys.argv[1]) <= 12:
+if len(sys.argv) > 2 and int(sys.argv[1]) >= 1 and int(sys.argv[1]) <= 12:
     yy = int(sys.argv[2])
     mm = int(sys.argv[1])
 elif len(sys.argv) > 1 and int(sys.argv[1]) >= 1 and int(sys.argv[1]) <= 12:
