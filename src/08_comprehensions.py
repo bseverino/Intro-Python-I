cc1a1859acf3a833a416f51040e3323be75e5540
bseverino <biancasev@gmail.com>
2020-04-07 15:40:50 -0400

    Fixed list comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,18 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for x in range(5):
-    y.append(x + 1)
+y = [x+1 for x in range(5)]
 
 print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for x in range(10):
-    y.append(x**3)
+y = [x**3 for x in range(10)]
 
 print(y)
 
@@ -30,9 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-for x in a:
-    y.append(x.upper())
+y = [x.upper() for x in a]
 
 print(y)
 
@@ -40,16 +34,8 @@ print(y)
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
-even = []
-
-for val in x:
-    val = int(val)
-    if val % 2 == 0:
-        even.append(val)
-
-print(even)
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [int(val) for val in x if int(val) % 2 == 0]
 
 print(y)
