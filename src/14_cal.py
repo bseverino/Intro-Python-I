6768c13b8f73fa38bd08f9f1e68fd57f6c0bfbd8
bseverino <biancasev@gmail.com>
2020-04-07 11:45:00 -0400

    Added error handling to calendar

--- src/14_cal.py
+++ src/14_cal.py
@@ -34,11 +34,20 @@ from datetime import datetime
 yy = datetime.now().year
 mm = datetime.now().month
 
-if len(sys.argv) > 2 and int(sys.argv[1]) >= 1 and int(sys.argv[1]) <= 12:
-    yy = int(sys.argv[2])
-    mm = int(sys.argv[1])
-elif len(sys.argv) > 1 and int(sys.argv[1]) >= 1 and int(sys.argv[1]) <= 12:
-    mm = int(sys.argv[1])
-
-
-print(calendar.month(yy, mm))
+if len(sys.argv) > 2:
+    try:
+        if 1 <= int(sys.argv[1]) <= 12:
+            yy = int(sys.argv[2])
+            mm = int(sys.argv[1])
+            print(calendar.month(yy, mm))
+    except:
+        print('Please enter in the form of `14_cal.py [month] [year]`')
+elif len(sys.argv) > 1:
+    try:
+        if 1 <= int(sys.argv[1]) <= 12:
+            mm = int(sys.argv[1])
+            print(calendar.month(yy, mm))
+    except:
+        print('Please enter in the form of `14_cal.py [month] [year]`')
+else:
+    print(calendar.month(yy, mm))
